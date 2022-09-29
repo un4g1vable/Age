e7a1b57f7b37b540e144db9343739e6f202091f4
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-09-29 18:53:22 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+	setlocale(0, "");
+	int a;
+	std::cout << "Введите ваш возраст: ";
+	std::cin >> a;
+	if (a % 100 > 10 or a % 100 < 20)
+		std::cout << "Вам " << a << " лет";
+	else if ((a == 1) or (a % 10 == 1))
+		std::cout << "Вам " << a << " год";
+	else if ((a % 10 == 2) or (a % 10 == 3) or (a % 10 == 4))
+		std::cout << "Вам " << a << " года";
+	else
+		std::cout << "Вам " << a << " лет";
+}
